08808092e9897cf553ea523792c71e56f55de105
susanka068 <sathinandimajumder068@gmail.com>
2020-01-20 12:41:31 +0530

    q2.java

--- jucse/qq1.java
+++ jucse/qq1.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Scanner;
-public class q1
+public class qq1
 {
     public static void main(String[] args)
     {
